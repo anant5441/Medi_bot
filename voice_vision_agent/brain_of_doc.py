5ddde553067d7d7c1174292ff2eccabecd00ccee
Anant <anantji2332@gmail.com>
2026-02-03 00:03:36 +0530

    voice-vison agent ready

---
+++ voice_vision_agent/brain_of_doc.py
@@ -0,0 +1,43 @@
+import os
+from groq import Groq
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+
+import base64
+
+image_path="acne.jpg"
+
+def encode_image(image_path):   
+    image_file=open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+query="Is there something wrong with my face?"
+
+model="meta-llama/llama-4-scout-17b-16e-instruct"
+
+def analyze_image_with_query(query, model, encoded_image):
+    client=Groq()  
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text", 
+                    "text": query
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }]
+    chat_completion=client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
+
+    return chat_completion.choices[0].message.content
+
+print(analyze_image_with_query(query, model, encode_image(image_path)))
